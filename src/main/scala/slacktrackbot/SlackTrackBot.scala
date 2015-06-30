cf7df132da64050739a24e3ea661d5edfa7d5ba3
Daniel Wiklund <daniel@happyrabbit.com>
2015-06-30 12:47:24 +0800

    setup

--- src/main/scala/slacktrackbot/SlackTrackBot.scala
+++ src/main/scala/slacktrackbot/SlackTrackBot.scala
@@ -1,7 +1,22 @@
+import slack.rtm.SlackRtmClient
+import akka.actor.ActorSystem
+
 object SlackTrackBot {
 
   def main(args: Array[String]) {
     println("SlackTrack started!")
+
+    implicit val system = ActorSystem("slack")
+
+    val token = sys.env("SLACK_TOKEN")
+    val client = SlackRtmClient(token)
+
+    val state = client.state
+    val selfId = state.self.id
+    val chanId = state.getChannelIdForName("general")
+
+    val generalChanId = state.getChannelIdForName("general").get
+    client.sendMessage(generalChanId, "Hello!")
   }
 
 }
